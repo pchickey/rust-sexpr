d1286d7e842f19ef0ae4b2b2657d8634aa8544c3
Pat Hickey <pat@galois.com>
2015-09-12 16:44:02 -0700

    integers can be negative, cant have leading zeros, refactor test code, exports

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,10 @@ use sexpr_grammar::*;
 peg_file! sexpr_grammar("sexpr.rustpeg");
 
 
-fn list_vec(v : Vec<Sexpr>) -> Sexpr {
-    return Sexpr::List(Box::new(v));
+pub fn parse(s : &str) -> Result<ast::Sexpr, sexpr_grammar::ParseError> {
+    return sexpr_grammar::start(s);
 }
 
-fn list_singleton(e: Sexpr) -> Sexpr {
-    return list_vec(vec![e]);
-}
 
 pub fn main() {
     println!("{:?}", Sexpr::List(Box::new(vec![])));
@@ -22,43 +19,78 @@ pub fn main() {
     println!("{:?}", Sexpr::Float(3.14159));
     println!("{:?}", Sexpr::List(Box::new(vec![ Sexpr::Float(1.2345),
                                                 Sexpr::Integer(67890)])));
+}
 
-    assert_eq!(sexpr_grammar::atom("0"), Ok(Sexpr::Integer(0)));
-    assert_eq!(sexpr_grammar::atom("0.0"), Ok(Sexpr::Float(0.0)));
+#[cfg(test)]
 
-    assert_eq!(sexpr_grammar::start("(0.0)")
-               , Ok(list_singleton(Sexpr::Float(0.0))));
-    assert_eq!(sexpr_grammar::start("( 1.0)")
-               , Ok(list_singleton(Sexpr::Float(1.0))));
-    assert_eq!(sexpr_grammar::start("(2.0 )")
-               , Ok(list_singleton(Sexpr::Float(2.0))));
-    assert_eq!(sexpr_grammar::start("(1 2 3)")
-               , Ok(list_vec(vec![ Sexpr::Integer(1)
-                                 , Sexpr::Integer(2)
-                                 , Sexpr::Integer(3)
-                                 ])));
+mod test {
+
+    use super::ast::Sexpr;
+    use super::parse;
+    use sexpr_grammar;
+
+    fn list_vec(v : Vec<Sexpr>) -> Sexpr {
+        return Sexpr::List(Box::new(v));
+    }
+
+    fn list_singleton(e: Sexpr) -> Sexpr {
+        return list_vec(vec![e]);
+    }
+
+    #[test]
+    fn test_atoms() {
+        assert_eq!(sexpr_grammar::atom("0"), Ok(Sexpr::Integer(0)));
+        assert_eq!(sexpr_grammar::atom("1"), Ok(Sexpr::Integer(1)));
+        assert_eq!(sexpr_grammar::atom("10"), Ok(Sexpr::Integer(10)));
+        assert_eq!(sexpr_grammar::atom("-1"), Ok(Sexpr::Integer(-1)));
+        assert_eq!(sexpr_grammar::atom("0.0"), Ok(Sexpr::Float(0.0)));
+        assert_eq!(sexpr_grammar::atom("+1.0"), Ok(Sexpr::Float(1.0)));
+        assert_eq!(sexpr_grammar::atom("-0.0"), Ok(Sexpr::Float(0.0)));
+        assert_eq!(sexpr_grammar::atom("-1.0"), Ok(Sexpr::Float(-1.0)));
+
+        assert!(sexpr_grammar::atom("00000").is_err());
+        assert!(sexpr_grammar::atom("01").is_err());
+        assert!(sexpr_grammar::atom("01.1").is_err());
+
+    }
+
+    #[test]
+    fn test_lists() {
+        assert_eq!(parse("(0.0)")
+                   , Ok(list_singleton(Sexpr::Float(0.0))));
+        assert_eq!(parse("( 1.0)")
+                   , Ok(list_singleton(Sexpr::Float(1.0))));
+        assert_eq!(parse("(2.0 )")
+                   , Ok(list_singleton(Sexpr::Float(2.0))));
+        assert_eq!(parse("(1 2 3)")
+                   , Ok(list_vec(vec![ Sexpr::Integer(1)
+                                     , Sexpr::Integer(2)
+                                     , Sexpr::Integer(3)
+                                     ])));
 
 
-    assert_eq!(sexpr_grammar::start("((1))")
-               , Ok(list_singleton(list_singleton(Sexpr::Integer(1)))));
+        assert_eq!(parse("((1))")
+                   , Ok(list_singleton(list_singleton(Sexpr::Integer(1)))));
 
-    assert_eq!(sexpr_grammar::start("(((1)))")
-               , Ok(list_singleton(list_singleton(list_singleton(Sexpr::Integer(1))))));
+        assert_eq!(parse("(((1)))")
+                   , Ok(list_singleton(list_singleton(list_singleton(Sexpr::Integer(1))))));
 
-    assert_eq!(sexpr_grammar::start("( ((1 )))")
-               , Ok(list_singleton(list_singleton(list_singleton(Sexpr::Integer(1))))));
+        assert_eq!(parse("( ((1 )))")
+                   , Ok(list_singleton(list_singleton(list_singleton(Sexpr::Integer(1))))));
 
-    assert_eq!(sexpr_grammar::start("((1) 2 3)")
-               , Ok(list_vec(vec![ list_singleton(Sexpr::Integer(1))
-                                 , Sexpr::Integer(2)
-                                 , Sexpr::Integer(3)
-                                 ])));
+        assert_eq!(parse("((1) 2 3)")
+                   , Ok(list_vec(vec![ list_singleton(Sexpr::Integer(1))
+                                     , Sexpr::Integer(2)
+                                     , Sexpr::Integer(3)
+                                     ])));
 
-    assert!(sexpr_grammar::start("").is_err());
-    assert!(sexpr_grammar::start("(").is_err());
-    assert!(sexpr_grammar::start("1").is_err());
-    assert!(sexpr_grammar::start("(()").is_err());
-    assert!(sexpr_grammar::start("())").is_err());
+        assert!(parse("").is_err());
+        assert!(parse("(").is_err());
+        assert!(parse("1").is_err());
+        assert!(parse("(()").is_err());
+        assert!(parse("())").is_err());
+    }
+
 }
 
 #[test]
